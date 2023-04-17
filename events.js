6925ca906b50cdccf63f00a3be184a9cff29e93e
dangin <gingras_dan@hotmail.com>
2023-04-17 17:34:36 -0400

    3 files modified, 1 added

---
+++ events.js
@@ -0,0 +1,12 @@
+export const events = [];
+let e1 = {
+  id: '_' + crypto.randomUUID(),
+  class: 'Rogue',
+  name: 'Nalliah',
+  raid: 'Ulduar',
+  raidSize: '10',
+  date: '2023-04-18',
+  time: '20:00',
+  duration: 4,
+};
+events.push(e1);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import stylesheets
 import './style.css';
+import { events } from './events.js';
 
 // Write Javascript code!
 const calendar = document.getElementById('calendar');
@@ -42,13 +43,14 @@ days.forEach((d) => {
 
 const highlightDate = (e, _bool) => {
   let column = parseInt(
-    getComputedStyle(e.target.closest('.event')).gridColumn.split('/')[0]
+    getComputedStyle(e.target.closest('.event-anchor')).gridColumn.split('/')[0]
   );
   let elem = document.querySelector(`.day:nth-child(${column})`);
   if (elem) {
     elem.classList.toggle('highlight', _bool);
   }
 };
+
 document.querySelectorAll('.event').forEach((elem) => {
   console.log(elem.style.column);
   elem.addEventListener('mouseover', (e) => {
@@ -57,4 +59,24 @@ document.querySelectorAll('.event').forEach((elem) => {
   elem.addEventListener('mouseleave', (e) => {
     highlightDate(e, false);
   });
+});
+
+events.forEach((e) => {
+  let elem = document.createElement('div');
+  elem.classList.add('event-anchor');
+  elem.id = `${e.id}`;
+  let [h, m] = e.time.split(':');
+  elem.style.gridRow = `${h * 2 + m / 30} / span ${e.duration}`;
+  elem.style.gridColumn = '4';
+  elem.style.zIndex = '2';
+  elem.style.position = 'relative';
+  elem.innerHTML = `
+    <div class="event">
+      <div class="raid">${e.raid} ${e.raidSize}</div>
+      <div class="${e.class.toLowerCase()}">
+        <span class="characterName">${e.name}</span>
+      </div>
+    </div>
+  `;
+  calendar.append(elem);
 });
