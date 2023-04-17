d1ef18066d4fcf3f2042bb3da074ba16dc491fef
dangin <gingras_dan@hotmail.com>
2023-04-17 05:00:58 -0400

    3 files modified

--- index.js
+++ index.js
@@ -2,5 +2,39 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const calendar = document.getElementById('calendar');
+const year = new Date().getUTCFullYear();
+const month = (new Date().getUTCMonth() + 1).toString().padStart(2, 0);
+const day = new Date().getUTCDate().toString().padStart(2, 0);
+const weekday = new Date().getUTCDay();
+const count = 7;
+const offset = -1;
+const today = new Date(`${year}-${month}-${day}T00:00`);
+
+let days = [];
+for (let i = 0; i < count; i++) {
+  let _d = new Date();
+  _d.setTime(today.getTime());
+  _d.setDate(today.getDate() + i + offset);
+  days.push(_d);
+}
+const dayNames = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+calendar.style.setProperty('--days', count);
+
+days.forEach((d) => {
+  let elem = document.createElement('div');
+  elem.innerHTML = `
+  <div class="date">${d.getDate()}</div>
+  <div class="dayname">${dayNames[d.getDay()]}</div>
+  `;
+  elem.classList.add('day');
+  calendar.append(elem);
+});
